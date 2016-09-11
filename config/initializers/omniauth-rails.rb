2417aeddf91640a94840edec0cd3fcf24eb73802
Dan Rabinowitz <djr@DanielRabinowitz.com>
2016-09-11 14:03:31 -0600

    Add OmniAuth.config.test_mode

--- config/initializers/omniauth-rails.rb
+++ config/initializers/omniauth-rails.rb
@@ -1,9 +1,16 @@
+# TODO: Move this into the spec/ directory. It is not part of the Rails engine.
+require 'dotenv'
+Dotenv.load
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   config = {
-    client_id: 1,
-    client_secret: 2,
+    client_id: ENV["CLIENT_ID"],
+    client_secret: ENV["CLIENT_SECRET"],
   }
 
   provider :google_oauth2, config[:client_id], config[:client_secret],
     {:access_type => 'online', :approval_prompt => 'auto'}
 end
+
+OmniAuth.config.logger = Rails.logger
+OmniAuth.config.test_mode = true
--- spec/test_app/spec/requests/sessions_controller_spec.rb
+++ spec/test_app/spec/requests/sessions_controller_spec.rb
@@ -12,13 +12,35 @@ RSpec.describe Omniauth::Rails::SessionsController do
   end
 
   describe "#create" do
-    let(:email) { "foo@bar.com" }
-    let(:auth_response) { OpenStruct.new(info: OpenStruct.new(email: email) ) }
-    let(:env) { Hash["omniauth.auth" => auth_response] }
+    context "a response with invalid CSRF" do
+      around(:each) do |example|
+        OmniAuth.config.mock_auth[:google_oauth2] = :csrf_detected
+        example.run
+        OmniAuth.config.mock_auth[:google_oauth2] = nil
+      end
+
+      it "redirects to /auth/failure?message=csrf_detected&strategy=google_oauth2" do
+        get "/auth/google_oauth2/callback"
+        expect(response).to redirect_to("/auth/failure?message=csrf_detected&strategy=google_oauth2")
+      end
+    end
+
+    context "a response with invalid credentials" do
+      around(:each) do |example|
+        OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
+        example.run
+        OmniAuth.config.mock_auth[:google_oauth2] = nil
+      end
+
+      it "redirects to /auth/failure?message=invalid_credentials&strategy=google_oauth2" do
+        get "/auth/google_oauth2/callback"
+        expect(response).to redirect_to("/auth/failure?message=invalid_credentials&strategy=google_oauth2")
+      end
+    end    
 
     context "a valid auth response" do
       it "redirects to /" do
-        get "/auth/google_oauth2/callback", env: env
+        get "/auth/google_oauth2/callback"
         expect(response).to redirect_to("/")
       end
     end
