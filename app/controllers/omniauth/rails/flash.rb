1ba12a4247277d258eb6c01e85f8b832c9ff2431
Dan Rabinowitz <djr@DanielRabinowitz.com>
2016-09-12 23:37:18 -0600

    Remove unused code for setting url_to_return_to_after_authentication from params and referer

--- app/controllers/omniauth/rails/flash.rb
+++ app/controllers/omniauth/rails/flash.rb
@@ -5,38 +5,19 @@ module Omniauth
       extend ActiveSupport::Concern
 
       def set_url_to_return_to_after_authentication
-        # TODO: Sanitize these urls, to avoid phishing attacks
+        # Use caution when setting these urls.
+        # There are phishing risks associated with redirection, as described here:
         # See https://www.owasp.org/index.php/Unvalidated_Redirects_and_Forwards_Cheat_Sheet
         flash[:url_to_return_to_after_authentication] =
-          # url_to_return_to_after_authentication_from_params ||
           url_to_return_to_after_authentication_from_flash ||
-          # url_to_return_to_after_authentication_from_referer ||
           default_url_to_return_to_after_authentication
       end
 
       private
 
-      # def url_to_return_to_after_authentication_from_params
-      #   return nil unless allow_url_to_return_to_after_authentication_from_params?
-      #   params[:return_to]
-      # end
-
-      # def allow_url_to_return_to_after_authentication_from_params?
-      #   false
-      # end
-
       def url_to_return_to_after_authentication_from_flash
         flash[:url_to_return_to_after_authentication]
       end
-
-      # def url_to_return_to_after_authentication_from_referer
-      #   return nil unless allow_url_to_return_to_after_authentication_from_referer?
-      #   request.referer
-      # end
-
-      # def allow_url_to_return_to_after_authentication_from_referer?
-      #   false
-      # end
 
       def default_url_to_return_to_after_authentication
         Configuration.authenticated_root
